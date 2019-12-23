6394e6f8a2267651e414511acb226c95f29d03a3
Kirill Shcherbina <43553831+ar4ud1ksss@users.noreply.github.com>
2019-12-24 02:10:03 +0300

    Update 8sem.c

--- 8sem.c
+++ 8sem.c
@@ -1,40 +1,72 @@
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <sys/msg.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/sem.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
-char* Find(char* target, DIR* dir, char* stream);
+char* searching(int* n, char* curpath, char* search_name);
 
-int main(int depth, char* title[])
+int main(int argc, char *argv[])
     {
-    DIR* dir = opendir(".");
-    char* dis = (char*)calloc(17, sizeof(char));
-    dis = Find(title[1], dir, ".");
-    printf("%s\n", dis);
-    closedir (dir);
-    return 0;
+    DIR* dir = opendir("..");
+    int count = 0;
+    struct dirent *temp = readdir(dir);
+    struct stat buf;
+    char* search_name = argv[1];
+    int n = 0;
+    for(int i = 0; i < strlen(argv[2]); i++)
+	    {
+        n = argv[2][i] - '0';
+    	}
+    char* name = searching(&n, ".", search_name);
+    printf("name = %s\n", name);
     }
 
-char* Find(char* target, DIR* dir, char* stream)
+char* searching(int* n, char* curpath, char* search_name)
     {
-    struct dirent* entry;
-    struct stat buff;
-    char* name = (char*)calloc(17, sizeof(char));
-    while((entry = readdir(dir)) != NULL)
+    DIR* dir = opendir(curpath);
+    struct dirent *temp = readdir(dir);
+    char* name = (char*) calloc (17, sizeof(char));
+    struct stat buf;
+    while(temp)
         {
-        strcpy(name, stream);
+        strcpy(name, curpath);
         strcat(name, "/");
-        strcat(name ,entry->d_name);
-        stat(name, &buff);
-        if(S_ISREG(buff.st_mode) == 0 && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
-            return Find(target, dir, name);
-        if(strcmp(target, entry->d_name) == 0)
-            return name;
-            
+        strcat(name, temp -> d_name);
+        if(strcmp(temp -> d_name, ".") != 0 && strcmp(temp -> d_name, "..") != 0)
+            {
+            stat(name, &buf);
+            int type = 0;
+            if (S_ISREG(buf . st_mode))
+                {
+                if(strcmp(search_name , temp -> d_name) == 0)
+                    {
+                    return name;
+                    }
+                }
+            if(S_ISDIR(buf . st_mode))
+                {
+                if (*n > 0)
+                    {
+                    *n--;
+                    char* temp1 = NULL;
+                    temp1 = searching(n, name, search_name);
+                    if (temp1)
+                        return temp1;
+                    }
+                }
+            }
+            temp = readdir(dir);
         }
+        free(name);
     }
